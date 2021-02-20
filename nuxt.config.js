ff779b6b71bac4c026de8b6ca8fd7ecd430d5a70
supertorio <tori.kirk@gmail.com>
2021-02-20 16:29:53 +0100

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Albert Heijn Hamster Love',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    'nuxt-i18n',
+  ],
+  i18n: {
+    locales: [{code: 'en', file: 'en.js'},{code: 'nl',file: 'nl.js'}],
+    defaultLocale: 'en',
+    langDir: 'locales'
+  },
+  
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
